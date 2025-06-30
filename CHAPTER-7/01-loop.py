1615dbef43b16823fb66a879e4ec4ac4ff35e44f
Harsh Shaw <harshshaw983@gmail.com>
2025-06-30 21:29:59 +0530

    CHAPTER-07 Day 1

---
+++ CHAPTER-7/01-loop.py
@@ -0,0 +1,11 @@
+# Printing numbers from 1 to 5 using individual print statements
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+
+# The same task can be done more efficiently using a loop:
+# Using a for loop with the range() function to print numbers from 1 to 5
+for x in range(1, 6):  # range(1, 6) generates numbers from 1 to 5 (6 is excluded)
+  print(x)             # print each number in the range

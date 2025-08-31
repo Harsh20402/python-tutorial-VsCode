8c4c26da9a2d4354a985482f16a7bdaade09c5b3
Harsh Shaw <harshshaw983@gmail.com>
2025-08-31 20:21:14 +0530

    Add Final Chapter 02

--- CHAPTER-02/01-variables.py
+++ CHAPTER-02/01-variables.py
@@ -1,17 +1,17 @@
 # A variable is the name given to a memory location in a program.
 # It is used to store data values.
 
-a = 22          # 'a' is a variable that stores an integer value
-b = "Harsh"     # 'b' is a variable that stores a string value
-c = 22.2        # 'c' is a variable that stores a float value
+a = 22          # 'a' is a variable that stores an integer value.
+b = "Harsh"     # 'b' is a variable that stores a string value.
+c = 22.2        # 'c' is a variable that stores a float value.
 
 # Note:
 # - 'Keywords' are reserved words in Python like 'if', 'for', 'while', etc.
 # - 'Identifiers' are the names given to variables, functions, classes, etc.
 
-# Reassigning new values to variables
-a = 1           # assigning integer 1 to variable 'a'
-b = 2           # assigning integer 2 to variable 'b'
+# Reassigning new values to variables.
+a = 1           # assigning integer 1 to variable 'a'.
+b = 2           # assigning integer 2 to variable 'b'.
 
-# Printing the sum of variables a and b
+# Printing the sum of variables a and b.
 print(a + b)    # Output: 3
